26c460abec49d95d9b44f545a06ac1de40bbf9a9
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-11 19:31:20 +0200

    Testklasserne rettet til at teste hver klasse fremfor hver metode for at få et bedre overblik over hvilke test som er kørt

---
+++ KaffeNetCore.tests/FlatWhiteTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using kaffe;
+using KaffeNetCore.kaffe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace KaffeNetCore.tests
+{
+    [TestClass]
+    public class StyrkeTest
+    {
+        [TestMethod]
+
+        public void TestMethodFlatWhiteStyrke()
+        {
+            var flatWhite = new FlatWhite();
+
+            string styrke = flatWhite.Styrke();
+
+            Assert.AreEqual("Mild", styrke);
+
+        }
+
+        [TestMethod]
+
+        public void TestMethodSortKaffeStyrke()
+        {
+            var sortKaffe = new SortKaffe();
+
+            string styrke = sortKaffe.Styrke();
+
+            Assert.AreEqual("stærk", styrke);
+        }
+    }
+}
---
+++ KaffeNetCore.tests/SortKaffeTest.cs
@@ -0,0 +1,32 @@
+﻿
+using kaffe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KaffeNetCore.tests
+{
+    [TestClass]
+    public class SortKaffeTest
+    {
+        [TestMethod]
+
+        public void TestMethodSortKaffePris()
+        {
+            var sortKaffe = new SortKaffe();
+
+            int pris = sortKaffe.Pris();
+
+            Assert.AreEqual(20, pris);
+        }
+
+        [TestMethod]
+
+        public void TestMethodSortKaffeStyrke()
+        {
+            var sortKaffe = new SortKaffe();
+
+            string styrke = sortKaffe.Styrke();
+
+            Assert.AreEqual("stærk", styrke);
+        }
+    }
+}
