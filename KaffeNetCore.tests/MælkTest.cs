8580267d911824e1fea0f74e9815f9002e7b56c5
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-11 15:59:53 +0200

    Test klasse til mælk er oprettet

---
+++ KaffeNetCore.tests/MælkTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaffeNetCore.tests
+{
+    class MælkTest
+    {
+    }
+}
--- KaffeNetCore.tests/PrisTest.cs
+++ KaffeNetCore.tests/PrisTest.cs
@@ -7,13 +7,33 @@ namespace KaffeNetCore.tests
     public class PrisTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodLattePris()
         {
             var latte = new Latte();
 
             int pris = latte.Pris();
 
             Assert.AreEqual(40,pris);
+        }
+        [TestMethod]
+        public void TestMethodCortadoPris()
+        {
+            var cortado = new Cortado();
+
+            int pris = cortado.Pris();
+
+            Assert.AreEqual(25, pris);
+        }
+
+        [TestMethod]
+
+        public void TestMethodSortKaffePris()
+        {
+            var sortKaffe = new SortKaffe();
+
+            int pris = sortKaffe.Pris();
+
+            Assert.AreEqual(20, pris);
         }
     }
 }
